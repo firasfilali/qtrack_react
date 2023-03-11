0917aab22f8b1adc45ba6fa2d9cdc6b2de9e87da
moiMeme <ahmed.chebrak@gmail.com>
2023-03-11 11:24:01 +0100

    first commit

---
+++ production-management/src/test/java/com/eternal/wisdom/productionmanagment/ProductionManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eternal.wisdom.productionmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/eternal/wisdom/qtrack/QtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eternal.wisdom.qtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
