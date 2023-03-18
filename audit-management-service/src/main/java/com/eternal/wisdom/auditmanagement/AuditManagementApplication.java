e6cf07a523e1bbac4af4dd892650900951aafd4f
firasfilali <76403765+firasfilali@users.noreply.github.com>
2023-03-18 19:28:31 +0100

    QTRACK-4_fix_issues_related_to_last_commit

--- audit-management-service/src/main/java/com/eternal/wisdom/auditmanagement/AuditManagementApplication.java
+++ audit-management-service/src/main/java/com/eternal/wisdom/auditmanagement/AuditManagementApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuditManagementApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(AuditManagementApplication.class, args);
 	}
