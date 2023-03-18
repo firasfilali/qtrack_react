ee2eeacef0e97d150c7a8b7be4912f2716986260
firasfilali <76403765+firasfilali@users.noreply.github.com>
2023-03-18 19:21:32 +0100

    QTRACK-4_fix_issues_related_to_last_commit

--- discovery-server/src/main/java/com/eternal/wisdom/Main.java
+++
@@ -1,7 +0,0 @@
-package com.eternal.wisdom;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ notification-management-service/src/main/java/com/eternal/wisdom/notification/NotificationManagementApplication.java
@@ -0,0 +1,12 @@
+package com.eternal.wisdom.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationManagementApplication.class, args);
+    }
+
+}
