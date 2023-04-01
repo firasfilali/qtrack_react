c3010a3bb6cdada3f756c895efff99eb551e3dc6
firasfilali <76403765+firasfilali@users.noreply.github.com>
2023-04-01 22:20:17 +0200

    QTRACK-5-product-management-service-init

---
+++ production-management-service/app/src/main/java/com/eternal/wisdom/production/ProductionManagementApplication.java
@@ -0,0 +1,9 @@
+package com.eternal.wisdom.production;
+
+import org.springframework.boot.SpringApplication;
+
+public class ProductionManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductionManagementApplication.class,args);
+    }
+}
