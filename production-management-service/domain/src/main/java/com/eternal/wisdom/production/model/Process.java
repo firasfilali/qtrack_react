d23525d026ea76e1de46040dc1c877337fd198ba
firasfilali <76403765+firasfilali@users.noreply.github.com>
2023-04-04 02:19:40 +0200

    QTRACK-5-product-management-service-init/Process model

--- production-management-service/domain/src/main/java/com/eternal/wisdom/production/model/Process.java
+++ production-management-service/domain/src/main/java/com/eternal/wisdom/production/model/Process.java
@@ -1,4 +1,17 @@
 package com.eternal.wisdom.production.model;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
 public class Process {
+    private String id;
+    private String name;
+    private String description;
+    private LocalDate created_date;
+    private LocalDate updated_date;
+    private String status;
+    private List<Procedure> procedures;
 }
